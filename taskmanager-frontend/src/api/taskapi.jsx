63fcff7adabe8edbc65d7474a8e2fe45938ba74f
Raiko1189 <hirkowak123@gmail.com>
2025-11-01 11:53:11 -0700

    Update taskapi.jsx

--- taskmanager-frontend/src/api/taskapi.jsx
+++ taskmanager-frontend/src/api/taskapi.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/tasks";
-
+export const API_URL = "https://yourapp.onrender.com/api";
 export const getTasks = async () => {
   const response = await axios.get(API_URL);
   return response.data;
